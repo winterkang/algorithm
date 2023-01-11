0f6d50aab867c59afecdcd2ce0d6cbc15ac603d0
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-01-11 16:51:46 +0900

    [D1] Title: 대각선 출력하기, Time: 90 ms, Memory: 42,440 KB -BaekjoonHub

--- SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
@@ -1,1 +1,7 @@
-print("#++++","+#+++","++#++","+++#+","++++#", sep="\n")
+for i in range(5):
+    for k in range(5):
+        if i == k:
+            print("#", end="")
+        else:
+            print("+", end="")
+    print()
