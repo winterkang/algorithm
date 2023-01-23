9c9731c7842592788210c86273f69e03a2ed74b4
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-01-23 12:43:54 +0900

    [Bronze V] Title: 개수 세기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,5 +1,6 @@
-import sys
-
-T = int(sys.stdin.readline())  # 11
-N = list(map(int, sys.stdin.readline().split()))  # 11개의 숫자 입력
-print(N.count(int(sys.stdin.readline())))  # N 리스트에서 v를 찾아라
+N = int(input())
+for n in range(N):
+    num = list(map(int, input().split()))
+    v = int(input())
+    print(num.count(v))
+    break
