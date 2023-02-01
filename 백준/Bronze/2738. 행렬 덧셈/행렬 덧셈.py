c97019140981a077cc5e31bea35e0a2fe569468e
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-02-01 21:23:07 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,10 +1,21 @@
+# 틀린 코드
 N, M = map(int, input().split())  # 3 3
 
 A = [list(map(int, input().split())) for _ in range(N)]
-
-for i in range(N):  # 리스트의 원소(리스트)가 3개
-    B = list(map(int, input().split()))
-    # [3, 3, 3] [4, 4, 4] [5, 5, 100]
-    for j in range(M):  # 원소(리스트속 리스트의 원소)가 M개
-        print(A[i][j] + B[j], end=" ")
-    print()
+# range(N) 여기서 N인 이유는 문제에서 N개의 줄이라고 명시했으니까!
+#A = [
+#    [1, 1, 1],
+#    [2, 2, 2],
+#    [0, 1, 0]
+#]
+B = [list(map(int, input().split())) for _ in range(N)]
+#B = [
+#    [3, 3, 3],
+#    [4, 4, 4],
+#    [5, 5, 100]
+#]
+# A와 B의 합을 구하기
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end=" ")
+    print()  # 한 줄에 출력되는 것을 방지하고자 개행
