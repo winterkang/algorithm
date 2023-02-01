c99bb0176d35861ef3d4f16e5fbd1cf6388993cf
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-02-01 22:09:02 +0900

    [Bronze III] Title: 네 번째 점, Time: 308 ms, Memory: 131324 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.py
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -1,19 +1,20 @@
-# x, y축 리스트 만들고
-from collections import Counter
-X = []
-Y = []
+import statistics
+A = []
+B = []
+for _ in range(3):
+    a, b = map(int, input().split())
+    A.append(a)
+    B.append(b)
 
-for i in range(3):
-    x, y = list(map(int, input().split()))
-# 입력받은 수를 x, y에 저장하고
-    X.append(x)  # [5, 5, 7]
-    Y.append(y)  # [5, 7, 5]
-# x, y축에서 가장 적은 수 출력하기
-for i in X:
-    if X.count(i) == 1: # X리스트에서 5를 카운트하면 2개, 조건에 부합X
-        print(i, end=" ") # 7을 카운트하면 1개, 조건 부합 O
-        break
-for i in Y:
-    if Y.count(i) == 1:
-        print(i)
-        break
+A_most = statistics.mode(A)
+B_most = statistics.mode(B)
+for n in A:
+    if n == A_most:
+        pass
+    else:
+        print(n, end=" ")
+for n in B:
+    if n == B_most:
+        pass
+    else:
+        print(n)
