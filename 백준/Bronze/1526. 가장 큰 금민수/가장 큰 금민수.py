0bf5b3b83a3e2294737a95b8af002825fcaca039
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-02-11 21:46:30 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 296 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -1,9 +1,5 @@
-N = int(input())  # 100
-while True:
-    if len(str(N)) == str(N).count('4') + str(N).count('7'):
-        # 입력값 N을 string으로 형변환해서
-        # str(N)에 '4' + '7' 각각 카운트한 수의 합과 같다면
-        print(N)  # 출력
+n = int(input())
+for i in range(n, 3, -1):
+    if len(str(i)) == str(i).count('7') + str(i).count('4'):
+        print(i)
         break
-
-    N -= 1  # 만약 위의 조건이 맞지 않다면 -1
