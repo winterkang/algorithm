fea22bc0d90db86d28c4c503933e364575fca25c
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-02-12 18:28:07 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -3,11 +3,15 @@ donghuk = list(map(int, input().split()))
 
 new_chess = []
 
+# for i in range(6):
+#     if chess[i] - donghuk[i] == 0:
+#         new_chess.append(chess[i] - donghuk[i])
+#     elif chess[i] - donghuk[i] > 0:
+#         new_chess.append(chess[i] - donghuk[i])
+#     elif chess[i] - donghuk[i] < 0:
+#         new_chess.append(chess[i] - donghuk[i])
+# print(*new_chess)
+
 for i in range(6):
-    if chess[i] - donghuk[i] == 0:
-        new_chess.append(chess[i] - donghuk[i])
-    elif chess[i] - donghuk[i] > 0:
-        new_chess.append(chess[i] - donghuk[i])
-    elif chess[i] - donghuk[i] < 0:
-        new_chess.append(chess[i] - donghuk[i])
+    new_chess.append(chess[i] - donghuk[i])
 print(*new_chess)
