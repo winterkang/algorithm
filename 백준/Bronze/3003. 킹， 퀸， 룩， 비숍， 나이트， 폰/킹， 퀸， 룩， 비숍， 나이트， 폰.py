e696fcc6e2026a8a43ceb3a27ec6e31ea7222280
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-02-12 18:25:15 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,13 @@
+chess = [1, 1, 2, 2, 2, 8]
+donghuk = list(map(int, input().split()))
+
+new_chess = []
+
+for i in range(6):
+    if chess[i] - donghuk[i] == 0:
+        new_chess.append(chess[i] - donghuk[i])
+    elif chess[i] - donghuk[i] > 0:
+        new_chess.append(chess[i] - donghuk[i])
+    elif chess[i] - donghuk[i] < 0:
+        new_chess.append(chess[i] - donghuk[i])
+print(*new_chess)
