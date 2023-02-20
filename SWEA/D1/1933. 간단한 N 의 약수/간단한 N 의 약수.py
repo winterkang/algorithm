1dbf6eafb8192c6f7521c127058580dd583da949
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-02-21 00:06:46 +0900

    [D1] Title: 간단한 N 의 약수, Time: 142 ms, Memory: 58,260 KB -BaekjoonHub

--- SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.py
+++ SWEA/D1/1933. 간단한 N 의 약수/간단한 N 의 약수.py
@@ -1,15 +1,11 @@
 num = int(input())
 N = num
-cnt = 2
 new = [1, num]
 for i in range(2, num+1):
-    if N % cnt == 0:
-        new.append(cnt)
-        new.append(N // cnt)
-        cnt += 1
-    elif N % cnt != 0:
-        cnt += 1
-        pass
+    if N % i == 0:
+        new.append(i)
+        new.append(N // i)
+
 divisor = list(set(new))
 divisor.sort()
 print(*divisor)
