884f7091e0102df4ac9fe3dce846fee88cabe3ea
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-04-05 22:50:34 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 60.99 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    const n = Number(input[0]);
+    let triangle = ''
+    for (let i = 1; i <= n; i++){
+        triangle += '*'.repeat(i) + '\n'
+    }
+    console.log(triangle)
+});
