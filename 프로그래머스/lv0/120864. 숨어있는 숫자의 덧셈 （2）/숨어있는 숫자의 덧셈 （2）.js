e22db837379cae613bd2f73c6382f11c35b241be
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-06-07 11:52:30 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,5 +1,4 @@
 function solution(my_string) {
-    // my_string = "aAb1B2cC34oOp"
     let number = '';
     for (str of my_string){
         if (!isNaN(parseInt(str))){ // str을 정수로 변환했을 때 타입이 숫자가 아니라면 ~
@@ -8,13 +7,10 @@ function solution(my_string) {
             number += ' '
         }
     }
-    // console.log(number)    1 2  34   
-    const numberArray = number.split(' ').filter(item => item.length >= 1) 
-    // console.log(numberArray) [ '1', '2', '34' ]
-    if (numberArray.length === 0){
+    const numberArray = number.split(' ').filter(item => item.length >= 1) // 숫자만 필터링
+    if (numberArray.length === 0){ // 문자열에 자연수가 없는 경우 0을 return
         return 0
     }
-    const total = numberArray.map(item => parseInt(item)).reduce((acc, cur) => acc + cur)
-    // console.log(total) 37
+    const total = numberArray.map(item => parseInt(item)).reduce((acc, cur) => acc + cur) // 합 구하기
     return total;
 }
