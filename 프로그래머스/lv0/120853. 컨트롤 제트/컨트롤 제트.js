bd88ab7357e854de78114e0e91813e083b375a7d
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-06-10 18:47:22 +0900

    [level 0] Title: 컨트롤 제트, Time: 2.36 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,25 @@
+function solution(s) {
+    const array = s.split(' ') // 공백으로 구분하여 배열 변환
+
+    const arrNum = array.map(item => { // 정수와 'Z'조합으로 배열 생성
+        const num = parseInt(item)
+        return isNaN(num) ? item : num
+    })
+
+    // 'Z'의 인덱스를 찾아서 바로 앞의 값들의 합을 구하기
+    cnt = 0
+    for (let i = 0; i < arrNum.length; i++){
+        if (arrNum[i] === 'Z'){
+            cnt += arrNum[i-1]
+        }
+    }   
+    
+    if (!arrNum.includes('Z')){ // 'Z'가 없다면 바로 합을 구하고
+        const sum = arrNum.reduce((acc, cur) => acc + cur)
+        return sum
+    }else{ // 'Z'가 있다면 전체 합에서 cnt 빼기
+        const sumNum = arrNum.filter(item => typeof item === 'number').reduce((acc, cur) => acc + cur)
+        console.log(sumNum)
+        return sumNum - cnt
+    }
+}
