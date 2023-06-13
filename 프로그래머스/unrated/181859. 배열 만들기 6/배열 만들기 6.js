06c4b976230f430f2eb46789582d2897e4384d55
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-06-13 21:48:46 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.71 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,22 @@
+function solution(arr) {
+    var stk = [];
+    let i = 0
+    while(i < arr.length){
+        if (stk.length === 0){
+            stk.push(arr[i])
+            i++
+        }else if (stk.length > 0){
+            if (stk[stk.length - 1] === arr[i]){
+                stk.pop()
+                i++
+            }else{
+                stk.push(arr[i])
+                i++
+            }
+        }
+    }
+    if (stk.length === 0){
+        return [-1]
+    }
+    return stk;
+}
