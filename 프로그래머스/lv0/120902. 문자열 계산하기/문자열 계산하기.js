0a111d1807d8826de979c1ca06c463ddc1ad60ab
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-06-14 15:17:19 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,3 +1,16 @@
 function solution(my_string) {
-    return eval(my_string);
+    const str = my_string.split(' ')
+    
+    let stack = []
+    let operator = 1
+    for (let i = 0; i < str.length; i++){
+        if (str[i] === '+'){
+            operator = 1
+        }else if (str[i] === '-'){
+            operator = -1
+        }else{
+            stack.push(str[i] * operator)
+        }
+    }
+    return stack.reduce((acc, cur) => acc + cur)
 }
