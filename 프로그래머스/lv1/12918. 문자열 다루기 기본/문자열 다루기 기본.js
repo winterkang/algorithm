2e3913318a18777f2a09533d1c14fcfdc461d5fa
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-06-16 12:15:34 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,15 @@
+function solution(s) {
+    var answer = true;
+    const arr = s.split('')
+    if (arr.length === 4 || arr.length === 6){
+        for (let i = 0; i < arr.length; i++){
+            if (isNaN(Number(arr[i]))){
+                answer = false
+                break
+            }
+        }            
+    }else{
+        answer = false
+    }
+    return answer
+}
