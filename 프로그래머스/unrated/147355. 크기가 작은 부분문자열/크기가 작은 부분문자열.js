d5b8592f837442bcbf8b26f0cebe2e6b629f18ef
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-06-30 11:01:03 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,19 +1,17 @@
 function solution(t, p) {
-    const tArr = t.split('')
-    const len = p.length
-    let cnt = 0
-    let arr = []
-    for (let i = 0; i < tArr.length; i++){
-        arr.push(tArr.slice(i, len + cnt))
-        cnt++
-    }
-    const newArr = arr.filter(item => item.length === len)
-    let total = 0
-    for (let i = 0; i < newArr.length; i++){
-        if (parseInt(p) >= parseInt(newArr[i].join(''))){
-            total++
+    const tArr = t.split('');
+    const len = p.length;
+    let total = 0;
+    
+    let cnt = 0;
+    for (let i = 0; i < tArr.length; i++) {
+        if (tArr.slice(i, len + cnt).length === len) { // 필터링 조건을 첫 번째 반복문에서 수행
+            if (parseInt(p) >= parseInt(tArr.slice(i, len + cnt).join(''))) { // 두 번째 반복문도 첫 번째 반복문 안으로 이동
+                total++;
+            }
         }
+        cnt++;
     }
-    console.log(total)
+    
     return total;
 }
