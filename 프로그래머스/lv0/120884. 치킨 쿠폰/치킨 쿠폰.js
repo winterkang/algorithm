9ea3b8a13e85b26e1b9b3935392256305a40bea2
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-07-09 15:17:51 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -2,11 +2,16 @@ function solution(chicken) {
     var answer = 0;
     let coupon = chicken
     let eaten;
+    // console.log(`chicken 치킨의 수 : ${chicken}`)
     while(coupon >= 10){
         eaten = parseInt(coupon / 10)
         answer += eaten
         coupon = parseInt((coupon % 10) + eaten)
+        // console.log(`eaten 쿠폰으로 먹은 치킨의 수 : ${eaten}`)
+        // console.log(`coupon 가지고있는 쿠폰의 수 : ${coupon}`)
+        // console.log(`answer 서비스 치킨의 수 : ${answer}`)
+        // console.log('-------------------------------')
     }
-    // console.log(eaten, coupon, answer)
+    
     return answer;
 }
