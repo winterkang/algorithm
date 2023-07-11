4b7a690821f3b9b8f2bcce48918df7315f1fdd74
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-07-11 13:58:02 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 10.49 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,5 +1,4 @@
 function solution(s) {
-    const array = s.split('')
     var answer = [];
     let str = []
     for (let i = 0; i < s.length; i++){
@@ -7,13 +6,15 @@ function solution(s) {
             str.push(s[i])
             answer.push(-1)
         }else{ // 이미 str 배열에 같은 요소가 존재하는 경우
-            for (let j = str.length - 1; j >= 0; j--){ // str배열을 거꾸로 순회
-                if (s[i] === str[j]){
-                    answer.push(i - j)
-                    str.push(str[j])
-                    break // 가장 가까운 값을 찾으면 반복 중단
-                }
-            }
+            answer.push(i - str.lastIndexOf(s[i]))
+            str.push(s[i])
+            // for (let j = str.length - 1; j >= 0; j--){ // str배열을 거꾸로 순회
+            //     if (s[i] === str[j]){
+            //         answer.push(i - j)
+            //         str.push(str[j])
+            //         break // 가장 가까운 값을 찾으면 반복 중단
+            //     }
+            // }
         }
     }
     return answer;
