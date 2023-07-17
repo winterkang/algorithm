cd5470a4590405828863a3d2e0f08c294a2405b9
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-07-17 11:55:30 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,13 +1,14 @@
 function solution(lines) {
-    var answer = 0;
+    // 선분의 길이 체크
     let arr = []
     for (let i = 0; i < lines.length; i++){
         const [x, y] = lines[i]
-        for (let j = x + 1; j <= y; j++){
+        for (let j = x; j < y; j++){
             arr.push(j)
         }
     }
     // console.log(arr)
+    // 중복체크위한 객체 생성
     let obj = {}
     for (let i = 0; i < arr.length; i++){
         if (obj[arr[i]]){
@@ -16,12 +17,15 @@ function solution(lines) {
             obj[arr[i]] = 1
         }
     }
+    // console.log(obj)
+    // 중복된 key 찾기
     let keys = []
     for (const [key, value] of Object.entries(obj)){
         if (value >= 2){
             keys.push(key)
         }
     }
+    // console.log(keys)
     // console.log(keys.length)
     return keys.length;
 }
