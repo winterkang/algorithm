989850879c323ec3361646bb4232982b262c2dea
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-07-18 10:25:05 +0900

    [unrated] Title: 기사단원의 무기, Time: 2.60 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,24 @@
+function solution(number, limit, power) {
+    let divisor = [] // 1부터 number까지 각 숫자의 약수의 개수를 구한 배열
+    for (let i = 1; i <= number; i++){
+        let cnt = 0 // 해당 i의 약수의 개수
+        for (let j = 1; j <= Math.sqrt(i); j++){ // 시간초과 방지를 위해서 i제곱근까지 반복
+            if (i % j === 0){ // 나누어 떨어져도 약수
+                cnt++
+                if (i / j !== j){ // 나눈 값이 j가 아니어도 약수
+                    cnt++
+                }
+            }
+        }
+        divisor.push(cnt)
+    }
+    // divisor의 요소가 limit을 넘으면 power로 바꾸기
+    for (let i = 0; i < divisor.length; i++){
+        if (divisor[i] > limit){
+            divisor[i] = power
+        }
+    }
+    // divisor의 총합
+    const sum = divisor.reduce((acc, cur) => acc + cur, 0)
+    return sum;
+}
