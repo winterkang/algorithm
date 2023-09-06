90e8cadec40b2f67e6947674671b10d04f69e403
Daeun Kang <111983968+winterkang@users.noreply.github.com>
2023-09-06 12:58:57 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,11 +1,12 @@
 function solution(a, b, n) {
-    var answer = 0;
-    let emptyBottles = n
-    while(emptyBottles >= a){
-        const exchange = Math.floor(emptyBottles / a) * b
-        answer += exchange
-        emptyBottles = exchange + (emptyBottles % a)
+    let answer = 0;
+    let emptyBottles = n;  // 처음에는 가지고 있는 빈 병의 수가 n
+
+    while (emptyBottles >= a) { // 가지고 있는 빈 병의 수가 a보다 큰 경우에만 반복
+        // a개의 빈 병을 가져다주면 b개의 콜라를 받는다.
+        const exchangedCola = Math.floor(emptyBottles / a) * b;  
+        answer += exchangedCola;  // 받은 콜라 수를 누적
+        emptyBottles = exchangedCola + (emptyBottles % a);  // 남은 빈 병 수를 갱신
     }
-    
     return answer;
 }
